95657f9ceba8f74da071b4dd4571b51923386cca
habibaa74 <habibaahmeda242@gmail.com>
2025-12-22 19:06:57 +0200

    message routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/,messages", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
