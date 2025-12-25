66ac8e9ec40e28cef4dc0a439c15660b7b7da957
habibaa74 <habibaahmeda242@gmail.com>
2025-12-25 21:12:55 +0200

    online users implemented

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,38 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import { ENV } from "./env.js";
+import { socketAuthMiddleware } from "../middleware/socket.auth.middleware.js";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [ENV.CLIENT_URL],
+    credentials: true,
+  },
+});
+
+// apply authentication middleware to all socket connections
+io.use(socketAuthMiddleware);
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+// this for storing online users
+const userSocketMap = {}; //{userId:socketId}
+
+// WE USE HERE IO.ON BECAUSE THE SOCKET NOT DEFINED YET BUT AFTER IT DEFINED WE SHOULD USE SOCKET.ON TO LISTEN FOR EVENTS
+io.on("connection", (socket) => {
+  console.log("User connected", socket.user.fullName);
+  const userId = socket.userId;
+  userSocketMap[userId] = socket.id;
+  //io.emit() is used to send events to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  socket.on("disconnect", () => {
+    console.log("User disConnected", socket.user.fullName);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { io, app, server };
